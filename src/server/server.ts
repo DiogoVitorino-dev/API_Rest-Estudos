61fb5129e01ace4515ca65a90513cee26e1b4185
Diogo Vitorino Franca <diogovf90@hotmail.com>
2023-07-28 10:33:56 -0300

    added cors

--- src/server/server.ts
+++ src/server/server.ts
@@ -1,9 +1,14 @@
 import Express from 'express';
 import 'dotenv/config';
+import cors from 'cors';
 import './shared/services/TranslationsYup';
 import { router } from './routes';
 
 const server = Express();
+
+server.use(cors({
+	origin:process.env.ENABLED_CORS?.split(/\s+/) || []
+}));
 
 server.use(Express.json());
 
